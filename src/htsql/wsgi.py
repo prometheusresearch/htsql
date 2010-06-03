ec2337ab1d850dd0c29f268c01b8c0b630ebc37c
Kirill Simonov <xi@resolvent.net>
2010-06-03 02:15:30 -0400

    Added the module file `htsql.wsgi`.
    It was missed out from the previous commit.

---
+++ src/htsql/wsgi.py
@@ -0,0 +1,49 @@
+#
+# Copyright (c) 2006-2010, Prometheus Research, LLC
+# Authors: Clark C. Evans <cce@clarkevans.com>,
+#          Kirill Simonov <xi@resolvent.net>
+#
+
+
+"""
+This module provides a handler for WSGI requests.
+
+This module exports a global variable:
+
+`wsgi_adapters`
+    List of adapters declared in the module.
+"""
+
+from .adapter import Utility, weights, find_adapters
+from .tr.parser import QueryParser
+from .error import HTTPError
+import urllib
+
+
+class WSGI(Utility):
+    """
+    Handles WSGI requests.
+    """
+
+    def __call__(self, environ, start_response):
+        """
+        Implements the WSGI entry point.
+        """
+        # Parse and echo the query.
+        path_info = environ['PATH_INFO']
+        query_string = environ.get('QUERY_STRING')
+        uri = urllib.quote(path_info)
+        if query_string:
+            uri += '?'+query_string
+        parser = QueryParser(uri)
+        try:
+            syntax = parser.parse()
+        except HTTPError, exc:
+            return exc(environ, start_response)
+        start_response("200 OK", [('Content-Type', 'text/plain')])
+        return [str(syntax), "\n"]
+
+
+wsgi_adapters = find_adapters()
+
+
