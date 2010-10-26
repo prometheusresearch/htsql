d46e328de5b7081942feab3abd23d24b6e7673a5
zindel <zindel@prometheusresearch.com>
2010-10-26 15:54:58 +0200

    changing file paths

--- examples/htraf/static/htraf/htraf.js
+++ examples/htraf/static/htraf/htraf.js
@@ -1,10 +1,10 @@
-var prefix = HTRAF_PREFIX || '/';
+var prefix = HTRAF_PREFIX || '';
 if(!window.$) {
     document.write('<script type="text/javascript" '
-        + ' src="' + prefix + 'js/jquery-1.4.3.min.js"></script>\n');
+        + ' src="' + prefix + '/js/jquery-1.4.3.min.js"></script>\n');
 }
 
 if(!window.htraf) {
     document.write('<script type="text/javascript" '
-        + 'src="' + prefix + 'js/_htraf.js"></script>\n');
+        + 'src="' + prefix + '/js/_htraf.js"></script>\n');
 }
