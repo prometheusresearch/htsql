e79fe1686171bd16c999cb267a5e7b00aebd2a00
Kirill Simonov <xi@resolvent.net>
2011-01-20 10:51:12 -0500

    Documentation: added a custom CSS stylesheet.

--- doc/conf.py
+++ doc/conf.py
@@ -29,7 +29,7 @@ graphviz_output_format = 'svg'
 autodoc_member_order = 'bysource'
 
 # Add any paths that contain templates here, relative to this directory.
-#templates_path = ['_templates']
+templates_path = ['templates']
 
 # The suffix of source filenames.
 source_suffix = '.rst'
@@ -130,7 +130,7 @@ html_theme_options = {}
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-#html_static_path = ['_static']
+html_static_path = ['static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
