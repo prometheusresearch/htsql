6a9e8b4708bb7fe634024b693e88ac491932d7e6
Clark C. Evans <cce@clarkevans.com>
2011-02-08 22:00:47 -0500

    demo application of  htsql 2.0.0 working at http://ep.io http://htsql-demo.ep.io

--- demo/epio/install.py
+++ demo/epio/install.py
@@ -23,12 +23,3 @@ else:
     curr = None
     conn.commit()
     print "INSTALLED SCHEMA"
-
-# test that we can actually execute the wsgi app
-from htsql.application import Application
-connect = "pgsql://%(username)s:%(password)s@%(host)s:%(port)s/%(database)s"
-app = Application(connect % config['postgres'])
-
-from wsgiref import test
-test(app, request_method='GET', accept='text/plain',
-          path_info='/school')
--- demo/epio/wsgi.py
+++ demo/epio/wsgi.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 from bundle_config import config
-from htsql.application import Application
-connect = "pgsql://%(username)s:%(password)s@%(host)s:%(port)s/%(database)s"
-app = Application(connect % config['postgres'])
+from htsql import HTSQL
+conn = "pgsql://%(username)s:%(password)s@%(host)s:%(port)s/%(database)s"
+_app = HTSQL(conn % config['postgres'])
+def app(environ, start_response):
+    # epio wsgi server fails to handle iterators properly
+    return list(_app(environ, start_response))
