095495017e0ea3bb4da2ca72657bcb7336050bef
Kirill Simonov <xi@resolvent.net>
2011-09-14 19:16:41 -0400

    Fixed `*`-lookup under Oracle.

--- src/htsql_engine/oracle/tr/lookup.py
+++ src/htsql_engine/oracle/tr/lookup.py
@@ -4,21 +4,6 @@
 #
 
 
-from htsql.tr.lookup import ExpandTable
-from htsql.tr.syntax import IdentifierSyntax
-from htsql.tr.binding import ColumnRecipe
-
-
-class OracleExpandTable(ExpandTable):
-
-    def itemize_columns(self):
-        for column in self.binding.table.columns:
-            name = column.name
-            if name.isupper():
-                name = name.lower()
-            identifier = IdentifierSyntax(name, self.binding.mark)
-            link = self.find_link(column)
-            recipe = ColumnRecipe(column, link)
-            yield (identifier, recipe)
+# FIXME: convert uppercase object name to lower case.
 
 
