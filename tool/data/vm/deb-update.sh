663629ec3edc300210f55297ad0931e2047152de
Kirill Simonov <xi@resolvent.net>
2011-11-08 23:05:13 -0500

    Updated deb VM to build *.deb for python2.6 and python2.7.

--- tool/data/vm/deb-update.sh
+++ tool/data/vm/deb-update.sh
@@ -1,14 +1,31 @@
 #/bin/sh
 
-# Post-installation script for the Debian DEB Build.
+# Post-installation script for the Debian DEB BuildBot.
 set -ex
 
 # Update the hostname.
 echo "deb-vm" >/etc/hostname
 
-apt-get -qy install python-all
-apt-get -qy install debhelper
-apt-get -qy install python-setuptools
-apt-get -qy install python-yaml
+# Register the testing repository.
+echo "deb http://ftp.us.debian.org/debian/ wheezy main" >/etc/apt/sources.list.d/testing.list
+cat <<END >>/etc/apt/preferences.d/pinning.pref
+Package: *
+Pin: release n=wheezy
+Pin-Priority: 90
+END
+apt-get -q update
 
+# Install Python 2.7 dependencies from testing.
+APT_LISTCHANGES_FRONTEND=none \
+DEBIAN_FRONTEND=noninteractive \
+apt-get -qy install -t wheezy gcc-4.4
+
+# Install Python 2.7 and other build dependencies.
+apt-get -qy install -t wheezy python-all
+apt-get -qy install -t wheezy python-setuptools
+apt-get -qy install -t wheezy python-yaml
+apt-get -qy install debhelper
+
+# Clean APT cache.
 apt-get clean
+
