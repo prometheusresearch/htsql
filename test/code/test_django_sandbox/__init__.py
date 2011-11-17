e1e7d305a1cf4c1fe98d5e2bde8a89428f01d0f8
Kirill Simonov <xi@resolvent.net>
2011-11-17 02:26:41 -0500

    Added a sample django application for testing django addon.

---
+++ test/code/test_django_sandbox/__init__.py
@@ -0,0 +1,44 @@
+
+from .settings import DATABASES
+
+engine = {
+        'pgsql': 'postgresql_psycopg2',
+        'sqlite': 'sqlite3',
+}.get(sandbox.engine, sandbox.engine)
+DATABASES['default']['ENGINE'] += engine
+DATABASES['default']['NAME'] = sandbox.database
+if sandbox.username is not None:
+    DATABASES['default']['USER'] = sandbox.username
+if sandbox.password is not None:
+    DATABASES['default']['PASSWORD'] = sandbox.password
+if sandbox.host is not None:
+    DATABASES['default']['HOST'] = sandbox.host
+if sandbox.port is not None:
+    DATABASES['default']['PORT'] = str(sandbox.port)
+
+def createdb():
+    if sandbox.engine == 'mysql':
+        DATABASES['default']['OPTIONS'] = {
+                'init_command': 'SET storage_engine=INNODB',
+        }
+    from datetime import datetime
+    from django.core.management import call_command
+    from .polls.models import Poll, Choice
+    call_command('syncdb', verbosity=0)
+    p = Poll(question="What's up?", pub_date=datetime(2011, 01, 01))
+    p.save()
+    p.choice_set.create(choice='Not much', votes=10)
+    p.choice_set.create(choice='The sky', votes=20)
+    p.choice_set.create(choice='Just hacking again', votes=30)
+
+def dropdb():
+    from django.db import models, connections, transaction, DEFAULT_DB_ALIAS
+    from django.core.management import sql, color
+    app = models.get_app('polls')
+    connection = connections[DEFAULT_DB_ALIAS]
+    sql_list = sql.sql_delete(app, color.no_style(), connection)
+    cursor = connection.cursor()
+    for sql in sql_list:
+        cursor.execute(sql)
+    transaction.commit_unless_managed(using=DEFAULT_DB_ALIAS)
+
---
+++ test/code/test_django_sandbox/settings.py
@@ -0,0 +1,58 @@
+
+DEBUG = True
+
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = ()
+
+MANAGERS = ADMINS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+TIME_ZONE = 'America/Chicago'
+
+LANGUAGE_CODE = 'en-us'
+
+SITE_ID = 1
+
+USE_I18N = True
+
+USE_L10N = True
+
+MEDIA_ROOT = ''
+
+MEDIA_URL = ''
+
+STATIC_ROOT = ''
+
+STATIC_URL = '/static/'
+
+ADMIN_MEDIA_PREFIX = '/static/admin/'
+
+STATICFILES_DIRS = ()
+
+STATICFILES_FINDERS = ()
+
+SECRET_KEY = '^'
+
+TEMPLATE_LOADERS = ()
+
+MIDDLEWARE_CLASSES = ()
+
+ROOT_URLCONF = 'test_django_sandbox.urls'
+
+TEMPLATE_DIRS = ()
+
+INSTALLED_APPS = (
+    'test_django_sandbox.polls',
+)
+
