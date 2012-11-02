ac81b92ecb55bc4e2472b6e247d79acd1adf2efb
Kirill Simonov <xi@resolvent.net>
2012-11-02 00:17:06 -0500

    tweak.filedb: update plugin description.

--- src/htsql/tweak/filedb/__init__.py
+++ src/htsql/tweak/filedb/__init__.py
@@ -13,13 +13,26 @@ class TweakFileDBAddon(Addon):
 
     name = 'tweak.filedb'
     hint = """make a database from a set of CSV files"""
+    help = """
+    This addon makes a database from a set of CSV files. Each source
+    CSV file becomes a table in the database.  The name of the table
+    is derived from the file name; the column names are taken from
+    the first row of the CSV file.  The remaining rows become the
+    records in the table.
+
+    Parameter `sources` is a list of entries describing the source
+    files; each entry has the following fields:
+
+    `file`: the path to the CSV file.
+    """
 
     prerequisites = []
     postrequisites = ['htsql']
     parameters = [
             Parameter('sources', SeqVal(RecordVal([
                 ("file", StrVal())])),
-                default=[]),
+                default=[],
+                hint="""source CSV files"""),
     ]
 
     @classmethod
