beee5392187475a0483fd30d09641cd55845305f
Kirill Simonov <xi@resolvent.net>
2013-03-21 11:05:12 -0500

    Removed obsolete sphinxcontrib package; updated build scripts.

--- tool/data/vm/src-update.sh
+++ tool/data/vm/src-update.sh
@@ -12,13 +12,23 @@ apt-get -qy install mercurial
 # Install Python 2.6 and required Python packages.
 apt-get -qy install python2.6
 apt-get -qy install python-setuptools
+apt-get -qy install python-pip
 apt-get -qy install python-yaml
 
-# Documentation build dependencies.
-apt-get -qy install -t squeeze-backports python-sphinx
+# Install sphinxcontrib-texfigure dependencies.
 apt-get -qy install pgf
 apt-get -qy install poppler-utils
 apt-get -qy install netpbm
+
+# Install Sphinx and custom Sphinx extensions.
+pip -q install Sphinx
+pip -q install sphinxcontrib-htsql
+pip -q install sphinxcontrib-texfigure
+
+# Normalize the name of `rst2*` scripts.
+for filename in /usr/local/bin/rst2*.py; do
+    ln -s $filename ${filename%.*};
+done
 
 # Clean APT cache.
 apt-get clean
